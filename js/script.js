0fa66cf437e00d4eefe9383f59b27c98f371a125
Edison Matos <edison.matos@live.com>
2023-12-22 13:04:01 -0300

    feat(structure): New structure

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
 function validateLogin() {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+  // Resetando mensagens de erro
+  document.getElementById("emailError").textContent = "";
+  document.getElementById("passwordError").textContent = "";
 
-  if (email === "" || password === "") {
-    alert("Por favor, preencha todos os campos.");
-  } else {
-    // Aqui você pode adicionar lógica para verificar o login no backend
-    alert("Login bem-sucedido!");
+  // Removendo a classe de destaque dos campos
+  document.getElementById("email").classList.remove("error-field");
+  document.getElementById("password").classList.remove("error-field");
+
+  // Obtendo valores dos campos
+  var email = document.getElementById("email").value.trim();
+  var password = document.getElementById("password").value.trim();
+
+  // Validando se os campos estão vazios
+  if (email === "") {
+    document.getElementById("email").classList.add("error-field");
+    document.getElementById("emailError").textContent = "Preencha este campo";
   }
+
+  if (password === "") {
+    document.getElementById("password").classList.add("error-field");
+    document.getElementById("passwordError").textContent =
+      "Preencha este campo";
+  }
+
+  // Verificando se não há erros
+  if (email !== "" && password !== "") {
+    // Realizar outras verificações ou ações conforme necessário
+    redirectToOptions();
+  }
+}
+
+function redirectToOptions() {
+  // Redirecionar para a página options.html
+  window.location.href = "options.html";
 }
